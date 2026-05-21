68526f58651f988cb4b524706b7333d5d7162a5c
“John <john@email.com>
2026-05-21 23:21:07 +0300

    + final bot

--- config.py
+++ config.py
@@ -4,4 +4,6 @@ import os
 load_dotenv()
 TOKEN = os.getenv("TOKEN")
 
+CHANNEL_ID = os.getenv("CHANNEL_ID")
+
 GROQ_API_KEY = os.getenv("GROQ_API_KEY")
--- reg_bot.py
+++ reg_bot.py
@@ -1,11 +1,11 @@
 import csv
 from datetime import datetime
 from utils import is_admin, is_owner, is_user, write_logs, write_admin_logs
-from config import TOKEN
+from config import TOKEN, CHANNEL_ID
 import asyncio
 import sqlite3
 
-from aiogram import Bot, Dispatcher
+from aiogram import Bot, Dispatcher, F
 from aiogram.filters import CommandStart
 from aiogram.filters import Command
 from aiogram.types import Message
@@ -44,6 +44,22 @@ CREATE TABLE IF NOT EXISTS owners (
 )
 """)
 
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS posts (
+
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+
+    user_id INTEGER,
+    text TEXT,
+    status TEXT
+
+)
+""")
+
+
+conn.commit()
+
+
 
 conn.commit()
 
@@ -63,7 +79,7 @@ async def do_start(message: Message):
 
     if user:
 
-        await message.answer("Ты уже зарегистрирован")
+        await message.answer("Сделать пост:\n/send текст")
 
         cursor.execute("""
         UPDATE users
@@ -219,36 +235,223 @@ async def remove_admin(message: Message):
     conn.commit()
 
 @dp.message(Command("send"))
+@dp.message(F.photo)
 async def send_confession(message: Message):
+
     if not is_user(message):
         await message.answer("Для начала напиши /start")
         return
 
     cursor.execute(
-        "SELECT telegram_id FROM admins")
+        "SELECT telegram_id FROM admins"
+    )
 
     admins = cursor.fetchall()
-    text = message.text.replace("/send", "").strip()
-    if not text:
-        await message.answer("Напишите текст для поста")
+
+    # PHOTO
+    if message.photo:
+
+        photo = message.photo[-1].file_id
+        text = message.caption.replace("/send", "").strip() if message.caption else None
+
+        cursor.execute(
+            """
+            INSERT INTO posts (user_id, text, status, photo)
+            VALUES (?, ?, ?, ?)
+            """,
+            (message.from_user.id, text, "pending", photo)
+        )
+
+        conn.commit()
+
+        post_id = cursor.lastrowid
+
+        caption = f"""
+    📩 Новый пост
+
+    POST ID: {post_id}
+
+    Username: @{message.from_user.username}
+    ID: {message.from_user.id}
+    """
+
+        if text:
+            caption += f"\n\nText:\n{text}"
+
+        for admin in admins:
+            await bot.send_photo(
+                admin[0],
+                photo=photo,
+                caption=caption
+            )
+
+        await message.answer("Пост отправлен на модерацию")
+
+    # TEXT
     else:
+
+        text = message.text.replace("/send", "").strip()
+
+        if not text:
+            await message.answer("Напишите текст для поста")
+            return
+
+        cursor.execute(
+            """
+            INSERT INTO posts (user_id, text, status, photo)
+            VALUES (?, ?, ?, ?)
+            """,
+            (message.from_user.id, text, "pending", None)
+        )
+
+        conn.commit()
+
+        post_id = cursor.lastrowid
+
         for admin in admins:
             await bot.send_message(
                 admin[0],
                 f"""
-            📩 Новый пост
+    📩 Новый пост
 
-            👤 Username: @{message.from_user.username}
-            🆔 ID: {message.from_user.id}
+    POST ID: {post_id}
 
-            💌 Text:
-            {text}
-            """
+    Username: @{message.from_user.username}
+    ID: {message.from_user.id}
+
+    Text:
+    {text}
+    """
             )
 
+        await message.answer("Пост отправлен на модерацию")
 
 
 
+
+@dp.message(Command("accept"))
+async def accept(message: Message):
+
+    if not is_admin(message):
+        await message.answer("У тебя нету прав!")
+        return
+
+    post_id = message.text.replace("/accept", "").strip()
+
+    if not post_id:
+        await message.answer("Введите ID поста")
+        return
+
+    post_id = int(post_id)
+
+    cursor.execute(
+        "SELECT * FROM posts WHERE id = ?",
+        (post_id,)
+    )
+
+    post = cursor.fetchone()
+
+    if not post:
+        await message.answer("Пост не найден")
+        return
+
+    if post[3] == "accepted":
+        await message.answer("Пост уже опубликован")
+        return
+
+    # PHOTO
+    if post[4]:
+
+        await bot.send_photo(
+            CHANNEL_ID,
+            photo=post[4],
+            caption=post[2] if post[2] else None
+        )
+
+    # TEXT
+    else:
+
+        await bot.send_message(
+            CHANNEL_ID,
+            post[2]
+        )
+
+    cursor.execute(
+        """
+        UPDATE posts
+        SET status = ?
+        WHERE id = ?
+        """,
+        ("accepted", post_id)
+    )
+
+    conn.commit()
+
+    await bot.send_message(
+        post[1],
+        f"""
+✅ Ваш пост был одобрен
+
+💌 Text:
+{post[2] if post[2] else "Фото без текста"}
+"""
+    )
+
+    await message.answer("Пост опубликован")
+
+
+@dp.message(Command("reject"))
+async def reject(message: Message):
+
+    if not is_admin(message):
+        await message.answer("У тебя нету прав!")
+        return
+
+    post_id = message.text.replace("/reject", "").strip()
+
+    if not post_id:
+        await message.answer("Введите ID поста")
+        return
+
+    post_id = int(post_id)
+
+    cursor.execute(
+        "SELECT * FROM posts WHERE id = ?",
+        (post_id,)
+    )
+
+    post = cursor.fetchone()
+
+    if not post:
+        await message.answer("Пост не найден")
+        return
+
+    if post[3] == "rejected":
+        await message.answer("Пост уже отклонен")
+        return
+
+    cursor.execute(
+        """
+        UPDATE posts
+        SET status = ?
+        WHERE id = ?
+        """,
+        ("rejected", post_id)
+    )
+
+    conn.commit()
+
+    await bot.send_message(
+        post[1],
+        f"""
+❌ Ваш пост был отклонен
+
+💌 Text:
+{post[2] if post[2] else "Фото без текста"}
+"""
+    )
+
+    await message.answer("Пост отклонен")
 
 
 
@@ -282,15 +485,18 @@ async def get_admin_info(message: Message):
     if not is_admin(message):
         await message.answer("У тебя нету прав!")
         return
-    await message.answer("/users - показывает число зарегистрированных пользователей\n"
-                         "/all_users - показывает данные всех пользователей\n"
-                         "/broadcast - делает рассылку сообщения")
+    await message.answer(
+    "/users - показывает число зарегистрированных пользователей\n"
+    "/all_users - показывает данные всех пользователей\n"
+    "/broadcast - делает рассылку сообщения"
+    )
+
+
 
 
 
 async def main():
-    await bot.delete_webhook(drop_pending_updates=True)
-    await dp.start_polling(bot, skip_updates=True)
+    await dp.start_polling(bot)
 
 
 asyncio.run(main())
