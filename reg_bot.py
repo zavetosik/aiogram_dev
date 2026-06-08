77e6f4c78b475c3e85460055fcb904f20b782238
draxc <sasacikota97@gmail.com>
2026-06-08 09:36:23 +0200

    + fix

--- reg_bot.py
+++ reg_bot.py
@@ -1,7 +1,9 @@
-
+import csv
+from datetime import datetime
 from utils import is_admin, is_owner, is_user, write_logs, write_admin_logs
 from config import TOKEN, CHANNEL_ID
 import asyncio
+import sqlite3
 from database import cursor, conn
 
 from aiogram import Bot, Dispatcher, F
@@ -11,6 +13,9 @@ from aiogram.types import Message
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
+
+conn = sqlite3.connect("bot.db")
+cursor = conn.cursor()
 
 
 cursor.execute("""
